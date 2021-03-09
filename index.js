75a674cc6b362d37f8674db773e33d33042a3d7e
zCaesar <suphawat@nexpie.com>
2021-03-09 11:09:35 +0700

    update auth

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const db = require('./src/db.module')
-db.connect(function (err) {
-    if (err) {
-        console.log('Error connecting to Db')
-        return
-    }
+// db.connect(function (err) {
+//     if (err) {
+//         console.log('Error connecting to Db')
+//         return
+//     }
 
-    console.log('Connection established')
+console.log('Connection established')
 
-    const things = require('./src/things/things.controller')
-    const roles = require('./src/roles/roles.controller')
+const things = require('./src/things/things.controller')
+const roles = require('./src/roles/roles.controller')
 
-    const auth = require('./src/auth.module')
+const auth = require('./src/auth.module')
 
-    app.use(function (req, res, next) {
-        let shared_token = req.query?.shared_token || req.body?.shared_token;
-        if (auth.verify(shared_token)) next()
-        else res.send('Unauthorized, Invalid Token.')
-    })
-
-    app.use(function (req, res, next) {
-        req.con = db
-        next()
-    })
-
-    app.use('/things', things)
-    app.use('/roles', roles)
-
-    app.listen(3000)
+app.use(function (req, res, next) {
+    let shared_token = req.query?.shared_token || req.body?.shared_token;
+    if (auth.verify(shared_token)?.exp > Math.floor(Date.now() / 1000)) next()
+    else res.send('Unauthorized, Invalid Token.')
 })
+
+app.use(function (req, res, next) {
+    req.con = db
+    next()
+})
+
+app.use('/things', things)
+app.use('/roles', roles)
+
+app.listen(3000)
+// })
